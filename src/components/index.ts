58bfacd8fd8a6e72c0f2d4c1c60268dc4493f391
Hakan <hakandrsnn@gmail.com>
2024-09-25 13:59:16 +0300

    first commit with build

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-﻿export {Contact} from "./Contact.tsx";
-export {Whatiuse} from "./Whatiuse.tsx";
-export {Projects} from "./projects/Projects.tsx";
-export {AboutMe} from "./about/AboutMe.tsx";
-export {Nav} from "./Nav.tsx";
-
